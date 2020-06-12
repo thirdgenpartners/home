bb6f83f8c288bb7d0a8aa7c827d86771d95d2668
Ethan Perez <ethan@ethanperez.com>
2020-06-11 23:48:43 -0500

    Add hamburger

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,142 @@
 import Head from 'next/head'
 import styled from 'styled-components';
+import { useState } from 'react';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: blue;
+`;
 
 export default function() {
   return (
-    <>
+    <Container>
       <Head>
         <title>Third Gen Partners</title>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
       </Head>
-      <Container>
-        <Title>Third Gen Partners</Title>
-        <Statement>Searching for a business making <CallOut>$1M</CallOut> in owner earnings</Statement>
-        <Emails>
-          <a href="mailto:buying@thirdgenpartners.com">buying@thirdgenpartners.com</a>
-        </Emails>
-      </Container>
+      <HamburgerNav />
+      {/* <Logo src="/tpg.svg" /> */}
+    </Container>
+  )
+}
+
+const HamburgerNav = () => {
+  const [hamOpen, setHamOpen] = useState(false);
+
+  return (
+    <>
+      <AllNavContainer isOpen={hamOpen}>
+        <NavItemList>
+          <NavItem>What we look for</NavItem>
+          <NavItem>Who we are</NavItem>
+          <NavItem>Refer a business</NavItem>
+          <NavItem>Contact us</NavItem>
+        </NavItemList>
+      </AllNavContainer>
+      <HamIcon isOpen={hamOpen} onClick={() => setHamOpen(!hamOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </HamIcon>
     </>
   )
 }
 
-const Container = styled.div`
+const AllNavContainer = styled.div<{ isOpen: boolean }>`
+  display: absolute;
   height: 100vh;
-  width: 100vw;
-  background-color: #C41E3A;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  text-align: center;
+  width: 400px;
+  transition: transform .5s ease-in-out;
+  transform: translateX(-400px);
+
+  ${props => props.isOpen && `
+    transform: translate(0);
+  `}
 `;
 
-const Title = styled.h1`
-  color: white;
-  font-size: 44px;
+const NavItemList = styled.ul`
+  list-style-type: none;
 `;
 
-const Statement = styled.h2`
-  color: white;
+const NavItem = styled.li`
+  font-size: 24px;
+  cursor: pointer;
+  font-family: 'Lato', sans-serif;
+  color: #030F4F;
+  margin-bottom: 20px;
+
+  &:hover {
+    text-decoration: underline;
+    text-decoration-color: #F6A000;
+  }
 `;
 
-const CallOut = styled.span`
-  color: black;
+const HamIcon = styled.div<{ isOpen: boolean }>`
+  width: 60px;
+  height: 45px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 50px;
+  transform: rotate(0deg) scale(.8);
+  transition: .5s ease-in-out;
+  cursor: pointer;
+
+  ${props => props.isOpen && `
+    transform: rotate(0deg) translateX(270px) scale(.5) translateY(-60px);
+  `}
+
+  span {
+    display: block;
+    position: absolute;
+    height: 9px;
+    width: 100%;
+    background: #030F4F;
+    border-radius: 3px;
+    opacity: 1;
+    left: 0;
+    transform: rotate(0deg);
+    transition: .5s ease-in-out;
+
+    &:nth-child(1) {
+      top: 0px;
+      transform-origin: left center;
+
+      ${props => props.isOpen && `
+        transform: rotate(45deg);
+        top: -3px;
+        left: 8px;
+      `}
+    }
+
+    &:nth-child(2) {
+      top: 18px;
+      transform-origin: left center;
+
+      ${props => props.isOpen && `
+        width: 0%;
+        opacity: 0;
+      `}
+    }
+
+    &:nth-child(3) {
+      top: 36px;
+      transform-origin: left center;
+
+      ${props => props.isOpen && `
+        transform: rotate(-45deg);
+        top: 39px;
+        left: 8px;
+      `}
+    }
+  }
+`;
+
+const Logo = styled.img`
+  height: 100%;
+  width: 500px;
 `;
 
 const Emails = styled.h2`
